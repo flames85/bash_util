a3072a4acb71592e11b9e8f4ab6d0609d3b5ffdc
shaoqi <shaoqi@hyc.cn>
2017-04-29 16:12:28 +0800

    add grep

---
+++ docker/grep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# != 2 ] ; then
+echo -e "\033[32m USAGE: $0 <container-wildcard> <keywords> \033[0m"
+echo -e "\033[32m e.g.:  $0 mds "call id" \033[0m"
+exit 1;
+fi
+
+# 获取容器id
+container=`docker ps | grep $1 | head -n 1 | awk '{print $1}'`
+if [ -z $container ] ; then
+echo -e "\033[31m container not exists which image=<$1> \033[0m"
+exit 1;
+fi
+
+docker logs $container | grep $2 --color -n
+
+
+# 直接打开容器日志
+
