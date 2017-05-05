736ab6b667ab96ee07f517cc6b56a41463d1244a
shaoqi <shaoqi@hyc.cn>
2017-05-05 09:51:00 +0800

    修复bug

--- docker/stats.sh
+++ docker/stats.sh
@@ -8,7 +8,7 @@ fi
 
 # 获取容器id
 container=`docker ps | grep $1 | head -n 1 | awk '{print $1}'`
-if [ -z $container ] ; then
+if [ -z "$container" ] ; then
 echo -e "\033[31m container not exists which image=<$1> \033[0m"
 exit 1;
 fi
