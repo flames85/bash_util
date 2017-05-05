41586ffc972bcd54904815a1913d60b9fe854230
shaoqi <shaoqi@hyc.cn>
2017-05-05 11:13:58 +0800

    fix 注释

--- kubectl/where_is_pod.sh
+++ kubectl/where_is_pod.sh
@@ -5,7 +5,7 @@ echo -e "\033[32m e.g.:  $0 mds\033[0m"
 exit 1;
 fi
 
-# 获取容器id
+# 获取pod name
 name=`kubectl get pods | grep $1 | awk '{print $1}'`
 if [ -z "$name" ] ; then
 echo -e "\033[31m pod not exists which <pod-wildcard>=$1 \033[0m"
