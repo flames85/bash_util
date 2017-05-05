821e3e6a8c16e21a02a6e250fc9888990429479b
shaoqi <shaoqi@hyc.cn>
2017-05-05 15:59:07 +0800

    modify tail

--- docker/tail.sh
+++ docker/tail.sh
@@ -12,7 +12,7 @@ echo -e "\033[31m container not exists which <container-wildcard>=$1 \033[0m"
 exit 1;
 fi
 
-docker logs -f --tail="10" $container | grep "$2" -A 5 -B 5
+docker logs -f --tail="10" $container | grep "$2" -A 5 -B 5 --color
 
 
 # 动态显示日志
