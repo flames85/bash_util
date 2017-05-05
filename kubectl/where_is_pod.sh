bad23de14434b93b6f7230567fb8601fafe6a7c4
shaoqi <shaoqi@hyc.cn>
2017-05-05 11:10:13 +0800

    where is支持全部显示

--- kubectl/where_is_pod.sh
+++ kubectl/where_is_pod.sh
@@ -1,16 +1,22 @@
-if [ $# != 1 ] ; then
-echo -e "\033[32m USAGE: $0 <pod-wildcard> \033[0m"
-echo -e "\033[32m e.g.:  $0 mds \033[0m"
+if [ $# -eq 0 ] ; then
+echo -e "\033[32m USAGE: $0 <pod-wildcard> <match-plus> [-]\033[0m"
+echo -e "\033[32m e.g.:  $0 mds -\033[0m"
+echo -e "\033[32m e.g.:  $0 mds\033[0m"
 exit 1;
 fi
 
 # 获取容器id
-container=`kubectl get pods | grep $1 | awk '{print $1}'`
-if [ -z "$container" ] ; then
+name=`kubectl get pods | grep $1 | awk '{print $1}'`
+if [ -z "$name" ] ; then
 echo -e "\033[31m pod not exists which <pod-wildcard>=$1 \033[0m"
 exit 1;
 fi
 
-kubectl describe pods $container | grep Node --color
+if [ "$2" == "-" ] ; then
+kubectl describe pods $name | grep -w -E "*|Node:.*|Name:.*" --color
+else
+echo -e "\033[32m$(kubectl describe pods $name | grep -w -E 'Node:|Name:') \033[0m"
+fi
 
 # 查询pod在哪个node
+
